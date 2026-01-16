fdfcfc5c499ca21247ac7afaaeb0b3a4b37aa724
Libby Tanner <libbytanner05@gmail.com>
2026-01-16 15:15:54 -0700

    Delete addMoves for KnightMoveCalculator, now uses default

--- shared/src/main/java/chess/KnightMoveCalculator.java
+++ shared/src/main/java/chess/KnightMoveCalculator.java
@@ -8,31 +8,15 @@ public class KnightMoveCalculator implements PieceMoveCalculator{
     public KnightMoveCalculator() {
     }
 
-    private void addMove(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int row, int col) {
-        if (validPosition(row, col)) {
-            ChessPosition newPosition = new ChessPosition(row, col);
-            ChessPiece myPiece = board.getPiece(myPosition);
-            ChessPiece otherPiece = board.getPiece(newPosition);
-            if (otherPiece != null) {
-                if (!otherPiece.getTeamColor().equals(myPiece.getTeamColor())) {
-                    ChessMove move = new ChessMove(myPosition, newPosition, null);
-                    moves.add(move);
-                }
-            } else {
-                ChessMove move = new ChessMove(myPosition, newPosition, null);
-                moves.add(move);
-            }
-        }
-    }
-
     private void changeRowsAddMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int rowChange, int colChange) {
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
+        boolean blocked = false;
         int newRow = row + rowChange;
         int newCol = col - colChange;
-        addMove(board, myPosition, moves, newRow, newCol);
+        addMove(board, myPosition, moves, newRow, newCol, blocked);
         newCol = col + colChange;
-        addMove(board, myPosition, moves, newRow, newCol);
+        addMove(board, myPosition, moves, newRow, newCol, blocked);
     }
 
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
