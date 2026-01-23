6f221f841484df91ca0efb30ad47297cc9b2e5fd
Libby Tanner <libbytanner05@gmail.com>
2026-01-23 14:23:32 -0700

    Change BishopMoveCalculator to track row and col simultaneously, eliminate hard coded directions

--- shared/src/main/java/chess/BishopMoveCalculator.java
+++ shared/src/main/java/chess/BishopMoveCalculator.java
@@ -11,34 +11,19 @@ public class BishopMoveCalculator implements PieceMoveCalculator {
 
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
         List<ChessMove> moves = new ArrayList<>();
+        int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
-        boolean blocked = false;
-        for (int newRow = row - 1; newRow >= 1; newRow--) {
-            int diff = Math.abs(row - newRow);
-            int newCol = col - diff;
-            blocked = addMove(board, myPosition, moves, newRow, newCol, blocked, null);
+        for (int[] direction : directions) {
+            boolean blocked = false;
+            int newRow = row + direction[0];
+            int newCol = col + direction[1];
+            while (validPosition(newRow, newCol) && !blocked) {
+                blocked = addMove(board, myPosition, moves, newRow, newCol, blocked, null);
+                newRow += direction[0];
+                newCol += direction[1];
+            }
         }
-        blocked = false;
-        for (int i = row - 1; i >= 1; i--) {
-            int diff = Math.abs(row - i);
-            int newCol = col + diff;
-            blocked = addMove(board, myPosition, moves, i, newCol, blocked, null);
-        }
-        blocked = false;
-        for (int i = row + 1; i <= 8; i++) {
-            int diff = Math.abs(row - i);
-            int newCol = col - diff;
-            blocked = addMove(board, myPosition, moves, i, newCol, blocked, null);
-
-        }
-        blocked = false;
-        for (int i = row + 1; i <= 8; i++) {
-            int diff = Math.abs(row - i);
-            int newCol = col + diff;
-            blocked = addMove(board, myPosition, moves, i, newCol, blocked, null);
-        }
-
         return moves;
     }
 }
