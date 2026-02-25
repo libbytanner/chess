765832be24f2fcb98b36eeb54a7d4362c9cdca25
Libby Tanner <libbytanner05@gmail.com>
2026-02-25 00:31:30 -0700

    implement register endpoint, mostly functional

---
+++ server/src/main/java/server/UserService.java
@@ -0,0 +1,26 @@
+package server;
+
+import dataaccess.AuthMemoryDAO;
+import dataaccess.DataAccessException;
+import dataaccess.ExistingUserException;
+import dataaccess.UserMemoryDAO;
+import model.AuthData;
+import model.RegisterRequest;
+import model.RegisterResult;
+import model.UserData;
+
+public class UserService {
+    UserMemoryDAO userDao = new UserMemoryDAO();
+    AuthMemoryDAO authDao = new AuthMemoryDAO();
+    public RegisterResult register(RegisterRequest request) throws ExistingUserException {
+        if (userDao.getUser(request.username()) != null) {
+            throw new ExistingUserException("User already exists");
+        }
+        UserData user = new UserData(request.username(), request.password(), request.email());
+        userDao.createUser(user);
+        String token = authDao.generateToken();
+        AuthData auth = new AuthData(token, request.username());
+        authDao.createAuth(auth);
+        return new RegisterResult(request.username(), token);
+    }
+}
