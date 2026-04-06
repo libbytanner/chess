4b4d801d6a72e92e2a49cc6e2565b4c6c42e45b1
Libby Tanner <libbytanner05@gmail.com>
2026-04-05 21:27:10 -0600

    Quality edits

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -5,8 +5,8 @@ import chess.ChessMove;
 public class MakeMoveCommand extends UserGameCommand {
     ChessMove move;
     public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
-        this.move = move;
         super(UserGameCommand.CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
     }
 
     public ChessMove getMove() {
